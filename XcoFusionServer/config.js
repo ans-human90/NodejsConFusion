23919bbdd953a020ee965e7a68d1bf2e58c3d2ef
ans-human90 <68585853+ans-human90@users.noreply.github.com>
2022-06-17 14:21:43 +0530

    nodejs

---
+++ XcoFusionServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "1970344889774414",
+    clientSecret: "f282da0fc43f1c525411a942b0f8ffa1",
+  },
+};
